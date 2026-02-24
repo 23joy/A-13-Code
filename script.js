914f009291f6e227e19a25359589705f0832b177
23joy <joyshreeroy288@gmail.com>
2026-02-24 03:03:22 -0800

    add buttons styles in my project

--- script.js
+++ script.js
@@ -1,9 +1,39 @@
+let InterviewList=[];
+let RejectedList=[];
+
 const TotalCount=document.getElementById("Total-Count")
 const InterviewCount=document.getElementById("Interview-Count")
 const RejectedCount=document.getElementById("Rejected-Count")
+
 const count=document.getElementById("buttons")
+const mainContainer=document.querySelector('main')
+const emptysection=document.getElementById("empty-section")
+
+// buttons id style
+let AllStylebtn=document.getElementById("All-btn")
+let InerterviewStylebtn=document.getElementById("Interview-btn")
+let RejectedStylebtn=document.getElementById("Rejected-btn")
 
 function allCardCount(){
     TotalCount.innerText=count.children.length
+    InterviewCount.innerText=InterviewList.length
+    RejectedCount.innerText=RejectedList.length
 }
 allCardCount()
+
+// toggol style function
+function toggolStyle(id){
+    // remove bg and text color all button
+    AllStylebtn.classList.remove('bg-[#3B82F6]','text-white')
+    InerterviewStylebtn.classList.remove('bg-[#3B82F6]','text-white')
+    RejectedStylebtn.classList.remove('bg-[#3B82F6]','text-white')
+
+    // add bg-white & text gray all button
+
+    AllStylebtn.classList.add('bg-white','text-[#64748B]')
+    InerterviewStylebtn.classList.add('bg-white','text-[#64748B]')
+    RejectedStylebtn.classList.add('bg-white','text-[#64748B]')
+
+    
+
+}
